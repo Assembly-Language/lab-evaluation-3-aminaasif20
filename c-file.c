815a0ee5003568da8c675468af0bea0c0207d731
aminaasif20 <aminaasif245@gmail.com>
2024-12-30 12:40:25 +0500

    cFile

--- c-file.c
+++ c-file.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
 
+
+//extern "C" void __stdcall asmfunc(void);
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void __stdcall asmfunc(int array[], int length, int *result);
+void __stdcall asmfunc(int array[],int length );
 
 #ifdef __cplusplus
 }
 #endif
 
+
 int main() {
     system("cls");
     int abc;
-    printf("assembly proc calling from C! \n");
-    getch();
-    int array[8] = {-1, -2, -3, 1, 2, 3, 4, 5};
-    int result = 0;
-
-    asmfunc(array, 8, &result); // assembly proc calling
-
-    getch();
-
-    printf("Sum of odd numbers: %d\n", result); // printing in C
-
+    printf("assembly proc calling from  from C! \n");
+getch();
+    int array[8] = {-1,-2,-3,1,2,3,4,5};
+    
+    
+     asmfunc(array,8); //assembly proc calling
+   
+   getch();
+    
+    
+    printf("back to  C! \n"); // printing in c
+    
+  
+    
     return 0;
 }
