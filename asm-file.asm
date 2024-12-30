afb533c92efee685103a65d00e346c8629314583
aminaasif20 <aminaasif245@gmail.com>
2024-12-30 11:54:34 +0500

    asm-file

--- asm-file.asm
+++ asm-file.asm
@@ -1,27 +1,35 @@
-
-
 INCLUDE Irvine32.inc
-.data
-;public asmfunc
-msg db "assebly procedure end now",0
 
+.data
+array sword 10, 2, 3, -5, 6, 7, -8, 3
+sum sdword ?      ; Variable to store the sum of odd numbers
 
 .code
-asmfunc PROC p1:DWORD, p2:DWORD
-    
-    push ebp
-    mov ebp,esp
-    mov eax, [ebp+12
-\] ; Move the first parameter into EAX
-    call writedec
-    call crlf
-    ;add eax, p2 ; Add the second parameter to EAX
-    call writedec
-    call crlf
-    mov edx, offset msg
-    call writestring
-    call crlf
-    
+asmfunc proc
+push esp
+mov ebp,esp
+;sub esp,4
+    mov esi, offset array    ; Load the address of the array into ESI
+    mov ecx, lengthof array  ; Load the length of the array into ECX
+    mov eax, 0               ; Clear EAX register (used for summing odd numbers)
+
+lo:
+    movsx ebx, word ptr [esi] ; Load the current element into EBX, sign-extended
+    test ebx, 1               ; Test if the least significant bit is set (odd number)
+    jz skip                   ; If it's even (LSB = 0), skip the addition
+
+    add eax, ebx              ; Add the odd number to the sum (in EAX)
+
+skip:
+    add esi, type array       ; Move to the next element in the array
+    loop lo                   ; Continue looping if ECX > 0
+
+    mov sum, eax              ; Store the sum in the 'sum' variable
+    call WriteInt             ; Print the sum
+    call Crlf                 ; Print a new line
+
     ret
-asmfunc ENDP
+    pop esp
+asmfunc endp
+
 end
